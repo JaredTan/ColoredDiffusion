3da87cb2fe9e6e79603e03186b78a3a23c7ceb6f
JaredTan <jared.e.tan@gmail.com>
2017-07-31 21:59:36 -0700

    water molecules floating around

--- bundle.js
+++ bundle.js
@@ -42,14 +42,333 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
+	var _sim_view = __webpack_require__(1);
+	
+	var _sim_view2 = _interopRequireDefault(_sim_view);
+	
+	var _simulation = __webpack_require__(2);
+	
+	var _simulation2 = _interopRequireDefault(_simulation);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
 	document.addEventListener("DOMContentLoaded", function () {
-	  var cancan = document.getElementById('canvas');
-	  var ctx = cancan.getContext('2d');
+	  var canvas = document.getElementById('canvas');
+	  var ctx = canvas.getContext('2d');
+	
+	  var simulation = new _sim_view2.default(new _simulation2.default(), ctx);
+	  simulation.start();
 	});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var SimView = function () {
+	  function SimView(simulation, ctx) {
+	    _classCallCheck(this, SimView);
+	
+	    this.simulation = simulation;
+	    this.ctx = ctx;
+	  }
+	
+	  _createClass(SimView, [{
+	    key: "start",
+	    value: function start() {
+	      var _this = this;
+	
+	      setInterval(function () {
+	        return _this.movement();
+	      }, Math.floor(1000 / 60));
+	      setInterval(function () {
+	        return _this.simulation.draw(_this.ctx);
+	      }, Math.floor(1000 / 60));
+	    }
+	  }, {
+	    key: "movement",
+	    value: function movement() {
+	      this.simulation.moveObjects();
+	      this.simulation.checkCollisions();
+	    }
+	  }]);
+	
+	  return SimView;
+	}();
+	
+	exports.default = SimView;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _water_mol = __webpack_require__(3);
+	
+	var _water_mol2 = _interopRequireDefault(_water_mol);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var Sim = function () {
+	  function Sim() {
+	    var DIM_X = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1000;
+	    var DIM_Y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1000;
+	    var NUM_MOLECULES = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 2000;
+	
+	    _classCallCheck(this, Sim);
+	
+	    this.DIM_X = DIM_X;
+	    this.DIM_Y = DIM_Y;
+	    this.waterMols = [];
+	
+	    while (this.waterMols.length < NUM_MOLECULES) {
+	      var newMol = this.addWater();
+	      for (var i = 0; i < this.waterMols.length - 1; i++) {
+	        if (newMol.isCollidedWith(this.waterMols[i])) {
+	          this.waterMols.pop();
+	        }
+	      }
+	    }
+	  }
+	
+	  _createClass(Sim, [{
+	    key: 'addWater',
+	    value: function addWater() {
+	      var waterMol = new _water_mol2.default();
+	      this.waterMols.push(waterMol);
+	      return waterMol;
+	    }
+	  }, {
+	    key: 'draw',
+	    value: function draw(ctx) {
+	      ctx.clearRect(0, 0, 1000, 1000);
+	      this.waterMols.forEach(function (mol) {
+	        return mol.draw(ctx);
+	      });
+	    }
+	  }, {
+	    key: 'moveObjects',
+	    value: function moveObjects() {
+	      this.waterMols.forEach(function (mol) {
+	        return mol.move();
+	      });
+	    }
+	  }, {
+	    key: 'checkCollisions',
+	    value: function checkCollisions() {
+	      for (var i = 0; i < this.waterMols.length - 1; i++) {
+	        for (var j = i + 1; j < this.waterMols.length; j++) {
+	          var mol1 = this.waterMols[i];
+	          var mol2 = this.waterMols[j];
+	          if (mol1.isCollidedWith(mol2)) {
+	            mol1.separateObjects(mol2);
+	            mol1.handleElasticCollision(mol2);
+	          }
+	        }
+	      }
+	    }
+	  }]);
+	
+	  return Sim;
+	}();
+	
+	exports.default = Sim;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _util = __webpack_require__(4);
+	
+	var _util2 = _interopRequireDefault(_util);
+	
+	var _mol = __webpack_require__(5);
+	
+	var _mol2 = _interopRequireDefault(_mol);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var COLORS = function COLORS() {
+	  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	};
+	
+	var RADIUS = function RADIUS() {
+	  return 5;
+	};
+	
+	var VELOCITY = 1;
+	
+	var WaterMol = function (_Mol) {
+	  _inherits(WaterMol, _Mol);
+	
+	  function WaterMol() {
+	    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	
+	    _classCallCheck(this, WaterMol);
+	
+	    options.color = '#D3D3D3';
+	    options.radius = RADIUS();
+	    options.pos = [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)];
+	    options.vel = [(Math.random() - 0.5) * VELOCITY, (Math.random() - 0.5) * VELOCITY];
+	    return _possibleConstructorReturn(this, (WaterMol.__proto__ || Object.getPrototypeOf(WaterMol)).call(this, options));
+	  }
+	
+	  return WaterMol;
+	}(_mol2.default);
+	
+	exports.default = WaterMol;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var Util = {
+	  newVel: function newVel(r1, r2, v1, v2) {
+	    var mass1 = r1 * r1 * Math.PI;
+	    var mass2 = r2 * r2 * Math.PI;
+	    return (v1 * (mass1 - mass2) + 2 * mass2 * v2) / (mass1 + mass2);
+	  },
+	  dist: function dist(x1, x2, y1, y2) {
+	    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	  }
+	};
+	
+	exports.default = Util;
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _util = __webpack_require__(4);
+	
+	var _util2 = _interopRequireDefault(_util);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var Mol = function () {
+	  function Mol(options) {
+	    _classCallCheck(this, Mol);
+	
+	    this.pos = options.pos;
+	    this.vel = options.vel;
+	    this.radius = options.radius;
+	    this.color = options.color;
+	  }
+	
+	  _createClass(Mol, [{
+	    key: 'draw',
+	    value: function draw(ctx) {
+	      ctx.fillStyle = this.color;
+	      ctx.beginPath();
+	
+	      ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, false);
+	      ctx.fill();
+	    }
+	  }, {
+	    key: 'move',
+	    value: function move() {
+	      this.pos[0] = (this.pos[0] + this.vel[0]) % 1000;
+	      this.pos[0] < 0 ? this.pos[0] += 1000 : this.pos[0];
+	      this.pos[1] = (this.pos[1] + this.vel[1]) % 1000;
+	      this.pos[1] < 0 ? this.pos[1] += 1000 : this.pos[1];
+	    }
+	  }, {
+	    key: 'isCollidedWith',
+	    value: function isCollidedWith(other) {
+	      var x1 = this.pos[0];
+	      var y1 = this.pos[1];
+	      var x2 = other.pos[0];
+	      var y2 = other.pos[1];
+	      var dist = _util2.default.dist(x1, x2, y1, y2);
+	      var r1 = this.radius;
+	      var r2 = other.radius;
+	      return dist <= r1 + r2;
+	    }
+	  }, {
+	    key: 'handleElasticCollision',
+	    value: function handleElasticCollision(other) {
+	      var thisOldX = this.vel[0];
+	      var thisOldY = this.vel[1];
+	      var otherOldX = this.vel[0];
+	      var otherOldY = this.vel[1];
+	      this.vel[0] = _util2.default.newVel(this.radius, other.radius, thisOldX, otherOldX);
+	      this.vel[1] = _util2.default.newVel(this.radius, other.radius, thisOldY, otherOldY);
+	      other.vel[0] = _util2.default.newVel(other.radius, this.radius, otherOldX, thisOldX);
+	      other.vel[1] = _util2.default.newVel(other.radius, this.radius, otherOldY, thisOldY);
+	    }
+	  }, {
+	    key: 'separateObjects',
+	    value: function separateObjects(other) {
+	      var x1 = this.pos[0];
+	      var y1 = this.pos[1];
+	      var x2 = other.pos[0];
+	      var y2 = other.pos[1];
+	      var distBetween = _util2.default.dist(x1, x2, y1, y2);
+	      var r1 = this.radius;
+	      var r2 = other.radius;
+	      var overlapDist = r1 + r2 - distBetween;
+	      var dx = (x2 - x1) / overlapDist;
+	      var dy = (y2 - y1) / overlapDist;
+	      this.pos[0] -= overlapDist / 16 * dx;
+	      this.pos[1] -= overlapDist / 16 * dy;
+	      other.pos[0] += overlapDist / 16 * dx;
+	      other.pos[1] += overlapDist / 16 * dy;
+	    }
+	  }]);
+	
+	  return Mol;
+	}();
+	
+	exports.default = Mol;
 
 /***/ })
 /******/ ]);
